6051660950de464dba8460aacb32ee31f016acd9
ridho237 <ahmadridwan227@gmail.com>
2024-04-27 07:59:52 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const { addNoteHandler, getALLNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getALLNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
